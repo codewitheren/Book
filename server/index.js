cbe03cf7cfa388690fbbb447ac696ce2d06c99c1
codewitheren <erenerdogan037@gmail.com>
2024-01-01 19:10:33 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("*", (req, res) => {
+    res.status(404).send("Not Found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
